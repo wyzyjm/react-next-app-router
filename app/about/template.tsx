d61f5071dc6600d156ce8eba6dc1ed2cac24340c
v_wangyongzhi <v_wangyongzhi>
2025-02-28 08:47:16 +0800

    feat: 修改网站字体

--- app/about/template.tsx
+++ app/about/template.tsx
@@ -1,7 +1,13 @@
 import React from "react"
+// 修改网站字体
+import { Coiny } from "next/font/google"
 
+const aBeeZee = Coiny({
+	weight: "400",
+	subsets: ["latin-ext"]
+})
 const Template = ({ children }: { children: React.ReactNode }) => {
-	return <div className='w-1/2 mx-auto'>{children}</div>
+	return <div className={`w-1/2 mx-auto ${aBeeZee.className}`}>{children}</div>
 }
 
 export default Template
